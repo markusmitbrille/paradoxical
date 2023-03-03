03253e25627514cbf0e429ad8ac01f6fb00a9f24
Markus Rezai <markus.rezai@gmail.com>
2023-03-03 23:25:38 +0100

    added pop, push extensions

---
+++ Paradoxical/Extensions/ListExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Paradoxical.Extensions;
+
+public static class ListExtensions
+{
+    public static void Push<T>(this List<T> collection, T item)
+    {
+        collection.Add(item);
+    }
+
+    public static T Pop<T>(this List<T> collection)
+    {
+        T item = collection.Last();
+        collection.Remove(item);
+
+        return item;
+    }
+}
--- Paradoxical/Extensions/ObservableCollectionExtensions.cs
+++ Paradoxical/Extensions/ObservableCollectionExtensions.cs
@@ -1,16 +1,15 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
-namespace Paradoxical.Extensions
+namespace Paradoxical.Extensions;
+
+public static class ObservableCollectionExtensions
 {
-    public static class ObservableCollectionExtensions
+    public static void AddRange<T>(this ObservableCollection<T> collection, IEnumerable<T> items)
     {
-        public static void AddRange<T>(this ObservableCollection<T> collection, IEnumerable<T> items)
+        foreach (T item in items)
         {
-            foreach (T item in items)
-            {
-                collection.Add(item);
-            }
+            collection.Add(item);
         }
     }
 }
