884a8c278e210a2aa0fee4abdc00917682051111
Markus Rezai <markus.rezai@gmail.com>
2023-07-13 16:58:26 +0200

    implemented multiline code columns via converter

---
+++ Paradoxical/Converters/CodeConverter.cs
@@ -0,0 +1,29 @@
+﻿using Paradoxical.Core;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Paradoxical.Converters;
+
+public class CodeConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not string text)
+        { return value; }
+
+        text = text.Replace(ParadoxText.NewParagraph, "\r\n");
+
+        return text;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not string text)
+        { return value; }
+
+        text = text.Replace("\r\n", ParadoxText.NewParagraph);
+
+        return text;
+    }
+}
