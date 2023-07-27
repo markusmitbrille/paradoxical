363d79ed0d40b78a1e2736430088b26e48bdb095
Markus Rezai <markus.rezai@gmail.com>
2023-07-27 20:43:28 +0200

    added optional conversion of null to bool

--- Paradoxical/Converters/NegationConverter.cs
+++ Paradoxical/Converters/NegationConverter.cs
@@ -6,13 +6,25 @@ namespace Paradoxical.Converters;
 
 public class NegationConverter : IValueConverter
 {
+    public bool ConvertNull { get; set; } = true;
+
     public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
+        if (value == null && ConvertNull == true)
+        {
+            return false;
+        }
+
         return value is bool b ? !b : null;
     }
 
     public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
     {
+        if (value == null && ConvertNull == true)
+        {
+            return false;
+        }
+
         return value is bool b ? !b : null;
     }
 }
