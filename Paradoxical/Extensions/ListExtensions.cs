bf80ad7ba68a304ed2a3ebab4b56b11f6aeeaad8
Markus Rezai <markus.rezai@gmail.com>
2023-07-27 04:16:50 +0200

    implemented, improved list extensions

--- Paradoxical/Extensions/ListExtensions.cs
+++ Paradoxical/Extensions/ListExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Documents;
 
 namespace Paradoxical.Extensions;
 
@@ -12,9 +13,47 @@ public static class ListExtensions
 
     public static T Pop<T>(this List<T> collection)
     {
-        T item = collection.Last();
-        collection.Remove(item);
+        T item = collection[^1];
+        collection.RemoveAt(collection.Count - 1);
 
         return item;
+    }
+
+    public static T Peek<T>(this List<T> collection)
+    {
+        T item = collection.Last();
+
+        return item;
+    }
+
+    public static T? PeekOrDefault<T>(this List<T> collection)
+    {
+        if (collection.Any() == false)
+        {
+            return default;
+        }
+
+        return Peek(collection);
+    }
+
+    public static void RemoveAll<T>(this List<T> collection, T obj)
+    {
+        collection.RemoveAll(item => EqualityComparer<T>.Default.Equals(item, obj));
+    }
+
+    public static void RemoveConsecutiveDuplicates<T>(this List<T> collection)
+    {
+        int i = 0;
+        while (i < collection.Count - 1)
+        {
+            if (EqualityComparer<T>.Default.Equals(collection[i], collection[i + 1]))
+            {
+                collection.RemoveAt(i + 1);
+            }
+            else
+            {
+                i++;
+            }
+        }
     }
 }
