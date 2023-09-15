dcb915e55cf73eec228301a154123281be8829e8
Markus Rezai <markus.rezai@gmail.com>
2023-09-15 22:25:12 +0200

    implemented portrait view (#109)

---
+++ Paradoxical/View/Details/PortraitView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace Paradoxical.View;
+
+public partial class PortraitView : UserControl
+{
+    public PortraitView()
+    {
+        InitializeComponent();
+    }
+}
